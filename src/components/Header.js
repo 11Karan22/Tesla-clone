24180db40f2183125e531c3df82b3368d0ddbe65
karan <kstkaranthakur03@gmail.com>
2022-02-06 17:13:48 +0530

    adding styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -48,3 +48,77 @@ function Header() {
     )
 }
 
+const Contianer=styled.div`
+min-height:60px;
+position:fixed;
+display:flex;
+align-items:center;
+justify-content:space-between;
+padding:0 20px;
+top:0;
+left:0;
+right:0;
+z-index:10;
+`
+const Menu=styled.div`
+display:flex;
+align-items:center;
+justify-content:center;
+flex:1;
+p{
+    font-weight:600;
+    text-transform:uppercase;
+    padding:0 10px;
+    flex-wrap:no-wrap;
+
+}
+@media(max-width:768px)
+{
+    display:none;
+}
+`
+const RightMenu=styled.div`
+display:flex;
+align-items:center;
+a{
+    font-weight:600;
+    text-transform:uppercase;
+    padding:0 10px;
+    flex-wrap:nowrap;
+}
+
+`
+const CustomMenu=styled(MenuIcon)`
+cursor:pointer;
+`
+const BurgerNav=styled.div`
+position:fixed;
+top:0;
+right:0;
+height:100vh;
+background:white;
+width:300px;
+z-index:100;
+list-style:none;
+padding:20px;
+display:flex;
+flex-direction:column;
+text-align:start;
+transform:${props => props.show ?'translateX(0)':'translateX(100%)'};
+transition: transform 0.2s  ;
+li{
+    padding:15px 0;
+    border-bottom:1px solid rgba(0,0,0,0.2);
+    a{
+        font-weight:600;
+    }
+}
+
+`
+const CustomClose=styled(CloseIcon)`
+`
+const CloseWrapper=styled.div`
+display:flex;
+justify-content:flex-end;
+`
+export default Header;
